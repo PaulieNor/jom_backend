989cce51c269c479802d8c4335cb0967771d2cfa
PaulieNor <paul.91.norman@gmail.com>
2024-05-19 23:01:35 +0100

    fix finally - nginx working

--- jombackend/urls.py
+++ jombackend/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('djoser.urls')),
-    path('api/v1/', include('djoser.urls.authtoken')),
-    path('api/v1/jobs/', include('job.urls'))
+    path('jom_be/admin/', admin.site.urls),
+    path('jom_be/api/v1/', include('djoser.urls')),
+    path('jom_be/api/v1/', include('djoser.urls.authtoken')),
+    path('jom_be/api/v1/jobs/', include('job.urls'))
 ]
